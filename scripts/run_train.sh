ce6346aeb4797f5b53c7a4c3ebacf1aa9c5d6713
lijian <lijian.hzau.m.s@gmail.com>
2025-11-17 08:47:40 +0000

    feat(gitignore): 添加 local_models 目录到 .gitignore 文件中 feat(run_train): 更新 run_train.sh 脚本以使用本地模型路径

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -7,7 +7,7 @@
 #        --dataset_name $1
 
 python sql_rl_gen/generation/sql_generation.py \
-       --model_name_or_path "juierror/flan-t5-text2sql-with-schema-v2" \
+       --model_name_or_path "./local_models/flan-t5-text2sql-with-schema-v2" \
        --dataset "example_text2sql_spider_train" \
        --template llama3 \
        --outdir "./output/model_spider_train" \
